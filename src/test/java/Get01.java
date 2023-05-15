a224e64a2e5541b3734b9814fd842d94717289d2
samet ozcelik <smtab142790@gmail.com>
2023-05-15 16:12:17 +0300

    15/05/2023

---
+++ src/test/java/Get01.java
@@ -0,0 +1,72 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class Get01 {
+    /*
+    1) Postman, manuel API testleri icin kullandik.
+    2) otomasyon testleri icin de Rest Assured Library kullanacagiz
+    3) Otomasyon testlerimizi yaparken asagidaki adimlari izliyoruz
+        a) Gereksinimleri anlamak,
+        b) Test Case yaziyoruz
+            i) Test Case yaziliminda "Gherkin" dilini kullanacağiz. Bizler yazilim diline hakim
+               olsakda ,karşimizdaki hakim olmayabilir.Gherkin ile yazilan testleri anlamak ta
+               zorluk cekmeyeceklerdi
+
+               -Given : On kosullar
+               -When  : Yapilacak aksiyonlar icin (get(),put(),post(),patch() ve delete())
+               -Then  : Istek yaptiktan (request gonderdikten sonra) dogrulama
+               -And   : Coklu islemlerde kullanacağiz
+
+        c) Test kodlarimizi yazmaya baslayacagız
+
+            i)   Set the URL,
+            ii)  Set the expected Data (beklenen datanin olusturulmasi post,put,patch)
+            iii) Type code to send request (talep göndermek icin kod yazimi)
+            iv)  Do Assertion (dogrulama yapmak)
+
+     */
+    /*
+    Given https://restful-booker.herokuapp.com/booking/344
+    When user sends a GET Request to the url
+    Then HTTP Status Code should be 200
+    And Content Type should be JSON
+    And Status Line should be HTTP/1.1 200 OK
+     */
+    @Test
+    public void get(){
+       //  i)   Set the URL
+        String url="https://restful-booker.herokuapp.com/booking/344";
+
+      //  ii)  Set the expected Data (beklenen datanin olusturulmasi post,put,patch)
+      // bizden post,put yada patch istenmedigi icin bu case de kullanmayacagiz
+
+      //  iii) Type code to send request (talep göndermek icin kod yazimi)
+        Response response=given().when().get(url);
+       // response.prettyPrint();
+
+        //iv)  Do Assertion (dogrulama yapmak)
+
+        response.then().assertThat().statusCode(200).contentType("application/json").statusLine("HTTP/1.1 200 OK");
+
+        //Status Code Konsola yazdiralim
+        System.out.println("Status Code : " +response.getStatusCode());
+
+        // Content Type konsola yazdiralim
+        System.out.println("Content Type : "+response.getContentType());
+
+        // Status Line konsola yazdiralim
+        System.out.println("Status Line : "+response.getStatusLine());
+
+        //Header konsola yazdiralim
+        System.out.println("Header : "+response.getHeader("Server"));
+
+        //Headers konsola yazdiralim
+        System.out.println("Headers : "+response.getHeaders());
+
+        // Time konsola yazdiralim
+        System.out.println("Time : "+response.getTime());
+    }
+
+}
---
+++ src/test/java/Get02.java
@@ -0,0 +1,42 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get02 {
+    /*
+    Given https://restful-booker.herokuapp.com/booking/2500
+    When User send a GET Request to the url
+    Then HTTP Status code should be 404
+    And Status Line should be HTTP/1.1 404 Not Found
+    And Response body contains "Not Found"
+    And Response body does not contain "HepsiBurada"
+    And Server is "Cowboy"
+     */
+    @Test
+    public void get01(){
+        //  i)   Set the URL
+        String url="https://restful-booker.herokuapp.com/booking/2500+";
+        //  ii)  Set the expected Data (beklenen datanin olusturulmasi post,put,patch)
+        // bizden post,put yada patch istenmedigi icin bu case de kullanmayacagiz
+
+        //  iii) Type code to send request (talep göndermek icin kod yazimi)
+        Response response=given().when().get(url);
+        //response.prettyPrint();
+
+        //iv)  Do Assertion (dogrulama yapmak)
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+
+        //Body Not Found iceriyor mu testi yapildi
+        assertTrue(response.asString().contains("Not Found"));
+
+        //Body nin HepsiBurada icermedigi test yapildi
+        assertFalse(response.asString().contains("HepsiBurada"));
+
+        //Server in Cowboy olup oldugunu test ediyoruz
+        assertEquals("Cowboy",response.getHeader("server"));
+
+
+    }
+}
