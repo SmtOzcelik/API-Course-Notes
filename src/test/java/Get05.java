d362442fde660d45bb849538a56aef7e3a4a8cdc
samet ozcelik <smtab142790@gmail.com>
2023-05-16 11:58:57 +0300

    16/05/2023

--- src/test/java/Get05.java
+++ src/test/java/Get05.java
@@ -1,15 +1,32 @@
-import org.testng.annotations.Test;
+import base_url.RestfulBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
 
-public class Get05{
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Get05 extends RestfulBaseUrl {
      /*
         Given https://restful-booker.herokuapp.com/booking
         When User sends get request to the URL
         Then Status code is 200
-        And Among the data there should be someone whose firstname is "Ali" and lastname is "Cengiz"
+        And Among the data there should be someone whose firstname is "Eric" and lastname is "Ericsson"
      */
     @Test
     public void get05(){
+        // 1- Set the Url
+        spec.pathParam("first","booking").queryParams("firstname","Sally","lastname","Ericsson");
 
+        // 2- Set the expected Data
+
+        // 3- Send The Request And get the Response
+        Response response=given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        // 4- Do assertion
+        assertEquals(200,response.getStatusCode());
+        assertTrue(response.asString().contains("bookingid"));
 
     }
 }
