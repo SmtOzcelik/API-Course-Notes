6658dee310bdcdb2ff4e7dda576bdd6f663a11e9
samet ozcelik <smtab142790@gmail.com>
2023-05-16 15:11:28 +0300

    16/05/2023

--- src/test/java/Get06.java
+++ src/test/java/Get06.java
@@ -1,25 +1,51 @@
 import base_url.RestfulBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Get06 extends RestfulBaseUrl {
     /*
-        Given https://restful-booker.herokuapp.com/booking/2325
+        Given https://restful-booker.herokuapp.com/booking/710
         When User send a GET request to the URL
         Then HTTP Status Code should be 200
         And Response content type is "application/json"
         And Response body should be like;
          {
-    "firstname": "Bradley",
-    "lastname": "Pearson",
-    "totalprice": 132,
-    "depositpaid": false,
-    "bookingdates": { ==> Outer JSON
-        "checkin": "2022-10-27", ==> Inner JSON
-        "checkout": "2022-11-07" ==> Inner JSON
+    "firstname": "Josh",
+    "lastname": "Allen",
+    "totalprice": 111,
+    "depositpaid": true,
+    "bookingdates": {           // outer json
+        "checkin": "2018-01-01", // inner json
+        "checkout": "2019-01-01"  // inner json
     },
-    "additionalneeds": "None"
+    "additionalneeds": "super bowls"
 }
+
      */
+    @Test
     public void get06(){
+        //1-Set the Url
+        spec.pathParams("first","booking","second",710);
+        //2-Set the expected data
+
+        //3-Send the Request and get the Response
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint()
+
+        // 4-Do assertions
+        response.then().assertThat().statusCode(200).
+                contentType(ContentType.JSON).
+                body("firstname",equalTo("Josh"),"lastname",equalTo("Allen"),
+                        "totalprice",equalTo(111),
+                        "depositpaid",equalTo(true),
+                        "bookingdates.checkin",equalTo("2018-01-01"),
+                        "bookingdates.checkout",equalTo("2019-01-01"),
+                        "additionalneeds",equalTo("super bowls"));
 
     }
 }
