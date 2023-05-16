2afb5989237caaa7bfd9ffe2edb2efd1c3f0c09a
samet ozcelik <smtab142790@gmail.com>
2023-05-16 15:40:37 +0300

    16/05/2023

--- src/test/java/Get06.java
+++ src/test/java/Get06.java
@@ -1,11 +1,14 @@
 import base_url.RestfulBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
 
 public class Get06 extends RestfulBaseUrl {
     /*
@@ -35,9 +38,10 @@ public class Get06 extends RestfulBaseUrl {
 
         //3-Send the Request and get the Response
         Response response=given().spec(spec).when().get("/{first}/{second}");
-        response.prettyPrint()
+        response.prettyPrint();
 
         // 4-Do assertions
+        //1.yol
         response.then().assertThat().statusCode(200).
                 contentType(ContentType.JSON).
                 body("firstname",equalTo("Josh"),"lastname",equalTo("Allen"),
@@ -46,6 +50,24 @@ public class Get06 extends RestfulBaseUrl {
                         "bookingdates.checkin",equalTo("2018-01-01"),
                         "bookingdates.checkout",equalTo("2019-01-01"),
                         "additionalneeds",equalTo("super bowls"));
+        //2.yol JsonPath class in kullanimi
+        JsonPath json=response.jsonPath();
+        assertEquals("Josh",json.getString("firstname"));
+        assertEquals("Allen",json.getString("lastname"));
+        assertEquals(111,json.getString("totalplace"));
+        assertEquals("2018-01-01",json.getString("bookingdates.checkin"));
+        assertEquals("2019-01-01",json.getString("bookingdates.checkout"));
+
+        // 3.yol softAssert --> 3 aşamada gercekleşir
+        // i) obje olusturma
+        SoftAssert softAssert=new SoftAssert();
+        // ii) Do Assertion(dogrula yapma)
+        softAssert.assertEquals(json.getString("fisrtname"),"Json","Firstname Hatali");
+        softAssert.assertEquals(json.getString("lastname"),"Allen","Lastname Hatali");
+        softAssert.assertEquals(json.getString("totalplace"),111,"Total Price Hatali");
+        softAssert.assertEquals(json.getString("bookingdates.checkin"),"2018-01-01","Check in Tarihi hatali");
+        // iii)
+        softAssert.assertAll();
 
     }
 }
