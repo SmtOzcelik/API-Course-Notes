b8b5923ba3b1b3d6e26efe98af4925d965af1a48
samet ozcelik <smtab142790@gmail.com>
2023-05-16 11:04:59 +0300

    16/05/2023

---
+++ src/test/java/Get05.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class Get05{
+     /*
+        Given https://restful-booker.herokuapp.com/booking
+        When User sends get request to the URL
+        Then Status code is 200
+        And Among the data there should be someone whose firstname is "Ali" and lastname is "Cengiz"
+     */
+    @Test
+    public void get05(){
+
+    }
+}
