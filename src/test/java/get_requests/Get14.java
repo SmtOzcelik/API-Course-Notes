eafcd62c5f4de6f799a6f121051f3aac8a17e42c
samet ozcelik <smtab142790@gmail.com>
2023-05-17 17:25:17 +0300

    17/05/2023

--- src/test/java/get_requests/Get14.java
+++ src/test/java/get_requests/Get14.java
@@ -1,7 +1,16 @@
 package get_requests;
 
 import base_url.JsonplaceholderBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class Get14 extends JsonplaceholderBaseUrl {
      /*
@@ -26,8 +35,41 @@ public class Get14 extends JsonplaceholderBaseUrl {
     @Test
     public void get(){
         //1-Set the url
-        spec.pathParams("first","todas","second",2);
+        spec.pathParams("first","todos","second",2);
         //2-Set the expected data
+        Map<String, Object>expectedData=new HashMap<>();
+        expectedData.put("userId",1);
+        expectedData.put("id",2);
+        expectedData.put("title","quis ut nam facilis et officia qui");
+        expectedData.put("completed",false);
+        System.out.println("expectedData : "+expectedData);
+
+        //3-Send the Request and get the Response
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //4-Do Assertion
+        // Status code is 200
+        assertEquals(200,response.statusCode());
+        // And header "Via" is "1.1 vegur"
+        assertEquals("1.1 vegur",response.getHeader("Via"));
+        // And header "Server" is "cloudflare"
+        assertEquals("cloudflare",response.getHeader("Server"));
+
+
+        Map<String,Object>actualData=response.as(HashMap.class);//De-Serialization yani json u java ya cevirdik
+        System.out.println("actualData : "+actualData);
+
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(expectedData.get("id"),actualData.get("id"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+
+        // Bu asagidaki ise önceki ögrendigimiz manuel karşılaştirdik
+        response.then().assertThat().body("userId",equalTo(1));
+        response.then().assertThat().body("id",equalTo(2));
+        response.then().assertThat().body("title",equalTo("quis ut nam facilis et officia qui"));
+        response.then().assertThat().body("completed",equalTo(false));
 
     }
 }
