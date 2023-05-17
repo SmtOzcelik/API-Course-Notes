babb8f697cce06ca0ffbbf62fdca95ce28155bfe
samet ozcelik <smtab142790@gmail.com>
2023-05-17 21:32:56 +0300

    17/05/2023

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -6,10 +6,10 @@ import java.util.Map;
 public class JsonPlaceHolderTestData {
     public Map<String,Object>expectedDataMetodu(Integer userId,String title,Boolean completed){
         Map<String, Object> expectedDataMap=new HashMap<>();
-        expectedDataMap.put("userId",1);
+        expectedDataMap.put("userId",userId);
 
-        expectedDataMap.put("title","quis ut nam facilis et officia qui");
-        expectedDataMap.put("completed",false);
+        expectedDataMap.put("title",title);
+        expectedDataMap.put("completed",completed);
         System.out.println("expectedDataMap : "+expectedDataMap);
 
         return expectedDataMap;
