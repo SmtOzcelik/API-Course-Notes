3edde7d3668bad7242fd934229e71f51014c4199
samet ozcelik <smtab142790@gmail.com>
2023-05-17 17:30:54 +0300

    17/05/2023

--- src/test/java/get_requests/Get14.java
+++ src/test/java/get_requests/Get14.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertEquals;
 
 public class Get14 extends JsonplaceholderBaseUrl {
