a7f3ed66b2d574389dfa63c81a87f7b5590a70dd
samet ozcelik <smtab142790@gmail.com>
2023-05-18 10:58:50 +0300

    18/05/2023

---
+++ src/test/java/get_requests/Get15.java
@@ -0,0 +1,23 @@
+package get_requests;
+
+public class Get15 {
+    /*
+      Given
+          https://restful-booker.herokuapp.com/booking/91
+      When
+          I send GET Request to the url
+      Then
+          Response body should be like that;
+           {
+    "firstname": "Josh",
+    "lastname": "Allen",
+    "totalprice": 111,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2018-01-01",
+        "checkout": "2019-01-01"
+    },
+    "additionalneeds": "super bowls"
+}
+   */
+}
