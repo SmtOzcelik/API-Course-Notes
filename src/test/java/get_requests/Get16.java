7204f4ef6bd14500ac33f8b509c2b3261fb5e561
samet ozcelik <smtab142790@gmail.com>
2023-05-18 12:19:35 +0300

    18/05/2023

--- src/test/java/get_requests/Get16.java
+++ src/test/java/get_requests/Get16.java
@@ -1,7 +1,15 @@
 package get_requests;
 
 import base_url.GorestBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.GorestTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Get16 extends GorestBaseUrl {
     /*
@@ -27,6 +35,19 @@ public class Get16 extends GorestBaseUrl {
         //1-Set the url
         spec.pathParams("first","users","second",29);
         //2-Set the expectedData
+        GorestTestData obj =new GorestTestData();
+        Map<String,String> dataKeyMap=obj.dataKeyMapMetod("Resource not found");
+        Map<String,Object>expectedData=obj.expectedDataMetodu(null,dataKeyMap);
+        System.out.println(expectedData);
+        //3-----
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+        //4----
+        Map<String,Object>actualDataMap=response.as(HashMap.class);
+        System.out.println(actualDataMap);
+        assertEquals(expectedData.get("meta"),actualDataMap.get("meta"));
+        assertEquals(dataKeyMap.get("message"),((Map)(actualDataMap.get("data"))).get("message"));
+
 
     }
 }
