80fc5d54921307cfff44c2fa8b3e602916d6161a
samet ozcelik <smtab142790@gmail.com>
2023-05-18 11:42:07 +0300

    18/05/2023

---
+++ src/test/java/get_requests/Get16.java
@@ -0,0 +1,22 @@
+package get_requests;
+
+public class Get16 {
+    /*
+        Given
+            https://gorest.co.in/public/v1/users/29
+        When
+            User send GET Request to the URL
+        Then
+            Status Code should be 200
+        And
+            Response body should be like
+            /*
+            {
+    "meta": null,
+    "data": {
+        "message": "Resource not found"
+    }
+}
+     */
+
+}
