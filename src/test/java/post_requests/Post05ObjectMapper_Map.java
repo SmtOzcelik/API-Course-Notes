2c215fd5a8726c8a54a6f237215e87b1e43209d9
samet ozcelik <smtab142790@gmail.com>
2023-05-20 18:13:10 +0300

    20/05/2023

---
+++ src/test/java/post_requests/Post05ObjectMapper_Map.java
@@ -0,0 +1,63 @@
+package post_requests;
+
+import base_url.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Post05ObjectMapper_Map extends JsonplaceholderBaseUrl {
+    /*
+         Given
+           1) https://jsonplaceholder.typicode.com/todos
+           2) {
+                 "userId": 55,
+                 "title": "Tidy your room",
+                 "completed": false
+               }
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+    @Test
+    public void post05() throws IOException {
+        //Set the url
+        spec.pathParam("1","todos");
+        //Set the expected data
+
+        String jsonInString="{\n" +
+                "                                    \"userId\": 55,\n" +
+                "                                    \"title\": \"Tidy your room\",\n" +
+                "                                    \"completed\": false,\n" +
+                "                                    \"id\": 201\n" +
+                "                                    }";
+
+        Map<String,Object>expectedData=new ObjectMapper().readValue(jsonInString, HashMap.class);
+        System.out.println("expectedData = " + expectedData);
+        //Send the request and get the response
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{1}");
+        response.prettyPrint();
+        //Do Assertions
+        Map<String,Object>actualData=new ObjectMapper().readValue(response.asString(),HashMap.class);
+        assertEquals(201,response.statusCode());
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+
+
+    }
+}
