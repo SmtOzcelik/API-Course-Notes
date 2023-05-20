2c5d0635eb16cd26517abf00cd9b889a9aba73f0
samet ozcelik <smtab142790@gmail.com>
2023-05-20 13:45:33 +0300

    20/05/2023

---
+++ src/test/java/pojos/BookingDatesPojo.java
@@ -0,0 +1,42 @@
+package pojos;
+
+public class BookingDatesPojo {
+    // 1-Tum key ler icin privite variable lar olusturuyoruz.
+    private String checkin;
+    private String checkout;
+    //2-Tum parametrelerle ve parametresiz constrocturlar olusturuyoz.
+
+    public BookingDatesPojo() {
+    }
+
+    public BookingDatesPojo(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+    //3-public getter ve setter methodlarini olusturuyoruz
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+    //4-toString methodunu olusturuyouz
+
+    @Override
+    public String toString() {
+        return "BookingDatesPojo{" +
+                "checkin='" + checkin + '\'' +
+                ", checkout='" + checkout + '\'' +
+                '}';
+    }
+}
