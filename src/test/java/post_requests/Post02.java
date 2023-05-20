697a54a1eb39877109343dbd3886620ca4e4b337
samet ozcelik <smtab142790@gmail.com>
2023-05-20 10:37:00 +0300

    20/05/2023

--- src/test/java/post_requests/Post02.java
+++ src/test/java/post_requests/Post02.java
@@ -1,10 +1,16 @@
 package post_requests;
 
 import base_url.RestfulBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.RestfulTestData;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Post02 extends RestfulBaseUrl {
     /*
@@ -43,7 +49,23 @@ public class Post02 extends RestfulBaseUrl {
         // Set the Url
         spec.pathParam("1","booking");
         //Set the expected data
+        RestfulTestData obj=new RestfulTestData();
+        Map<String,String>bookingdataMap=obj.bookingdatesMethod("2021-09-09","2021-09-21");
 
+        Map<String,Object>expectedDataMap=obj.expectedDataMethod("John","Doe",11111,true,bookingdataMap);
+        // Send the Resquest and get the response
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedDataMap).when().post("/{1}");
+        response.prettyPrint();
+
+        //Do Assertions
+        Map<String,Object>actualdataMap=response.as(HashMap.class);
+        assertEquals(expectedDataMap.get("firstname"),((Map)actualdataMap.get("booking")).get("firstname"));
+        assertEquals(expectedDataMap.get("lastname"),((Map)actualdataMap.get("booking")).get("lastname"));
+        assertEquals(expectedDataMap.get("totalprice"),((Map)actualdataMap.get("booking")).get("totalprice"));
+        assertEquals(expectedDataMap.get("depositpaid"),((Map)actualdataMap.get("booking")).get("depositpaid"));
+
+        assertEquals(bookingdataMap.get("checkin"),((Map)((Map)actualdataMap.get("booking")).get("bookingdates")).get("checkin"));
+        assertEquals(bookingdataMap.get("checkout"),((Map)((Map)actualdataMap.get("booking")).get("bookingdates")).get("checkout"));
 
 
     }
