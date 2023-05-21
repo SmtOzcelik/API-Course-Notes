14c2718e2b29b59271309fa8c40965d4557a7179
samet ozcelik <smtab142790@gmail.com>
2023-05-21 12:43:50 +0300

    20/05/2023

---
+++ src/test/java/get_requests/Get23.java
@@ -0,0 +1,30 @@
+package get_requests;
+
+import org.junit.Test;
+
+public class Get23 {
+    /*
+    bu soru pojo class ve ObjectMapper ile cözüm
+        Given
+	            https://restful-booker.herokuapp.com/booking/194
+        When
+		 		I send GET Request to the URL
+		Then
+		 		Status code is 200
+                  {
+    "firstname": "Jane",
+    "lastname": "Doe",
+    "totalprice": 111,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2018-01-01",
+        "checkout": "2019-01-01"
+    },
+    "additionalneeds": "Extra pillows please"
+}
+     */
+    @Test
+    public void get23(){
+
+    }
+}
