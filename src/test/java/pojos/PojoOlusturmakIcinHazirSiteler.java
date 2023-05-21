8076b9079b315038068518bab58bcccc363ba9f7
samet ozcelik <smtab142790@gmail.com>
2023-05-21 15:54:26 +0300

    20/05/2023

---
+++ src/test/java/pojos/PojoOlusturmakIcinHazirSiteler.java
@@ -0,0 +1,12 @@
+package pojos;
+
+/*
+    Bu class i olustururken verileri olusturan site
+    https://www.javainuse.com/pojo
+    kullandık.Ekleme olarak Construztor ve toString yaptik
+    Her zaman data isimlerini duzgün cevirmeye biliyor.
+    Daha kullanisli siteler keşfedilip tercih edilebilir.
+ */
+
+public class PojoOlusturmakIcinHazirSiteler {
+}
